06987a3b3b154f0ec2b4c54177284b9728056545
bangbangro <rocio.sanchez2@mail.udp.cl>
2025-09-05 18:48:17 -0400

    Create log.c

---
+++ log.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+struct Mensaje {
+    pid_t pid;        
+    char buf[256];  
+};
+
+struct Usuario {
+    pid_t pid;
+    int fd; 
+};
+
+int main(){
+
+    mkfifo("central.fifo", 0666);
+
+    int fd_central_in = open("central.fifo", O_RDONLY);
+
+struct Usuario usuarios[100];
+
+int num = 0; 
+
+struct Mensaje msg; 
+
+while (1)
+{
+    int n = (fd_central_in, &msg, sizeof(msg));
+
+    if(n>0){
+
+        for (int i = 0; i < num; i++)
+        {
+            if(usuarios[i].pid != msg.pid){
+                write(usuarios[i].fd, msg.buf, strlen(msg.buf));
+            }
+        }
+
+    }
+}
+
+close(fd_central_in);
+return 0;        
+    
+
+
+
+
+
+}
