d3509bb26d7dee1d523a5e46981cae1362b588d2
bangbangro <rocio.sanchez2@mail.udp.cl>
2025-09-08 19:48:44 -0300

    Update log.c

--- log.c
+++ log.c
@@ -15,40 +15,45 @@ struct Usuario {
     int fd; 
 };
 
-int main(){
-
+int main() {
     mkfifo("central.fifo", 0666);
 
     int fd_central_in = open("central.fifo", O_RDONLY);
 
-struct Usuario usuarios[100];
+    struct Usuario usuarios[100];
+    int num = 0; 
 
-int num = 0; 
+    struct Mensaje msg; 
 
-struct Mensaje msg; 
+    while (1) {
+        int n = read(fd_central_in, &msg, sizeof(msg));
 
-while (1)
-{
-    int n = read(fd_central_in, &msg, sizeof(msg));
+        if (n > 0) {
+            if (strcmp(msg.buf, "/register") == 0) {
+                // Nuevo usuario
+                char fifo_name[64];
+                sprintf(fifo_name, "user_%d.fifo", msg.pid);
+                mkfifo(fifo_name, 0666);
 
-    if(n>0){
+                int fd_user = open(fifo_name, O_WRONLY);
 
-        for (int i = 0; i < num; i++)
-        {
-            if(usuarios[i].pid != msg.pid){
-                write(usuarios[i].fd, msg.buf, strlen(msg.buf));
+                usuarios[num].pid = msg.pid;
+                usuarios[num].fd = fd_user;
+                num++;
+
+                printf("Usuario %d registrado\n", msg.pid);
+            } else {
+                // Reenviar mensaje a los demás
+                for (int i = 0; i < num; i++) {
+                    if (usuarios[i].pid != msg.pid) {
+                        write(usuarios[i].fd, msg.buf, strlen(msg.buf)+1);
+                    }
+                }
+                printf("Servidor reenvió: %s\n", msg.buf);
             }
         }
-
     }
-}
 
-close(fd_central_in);
-return 0;        
-    
-
-
-
-
-
+    close(fd_central_in);
+    return 0;
 }
