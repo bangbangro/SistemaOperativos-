2fcfab7478c61c9ee2dc1a010f950e4bb6010614
bangbangro <rocio.sanchez2@mail.udp.cl>
2025-09-10 19:19:01 -0300

    Update log.c

--- log.c
+++ log.c
@@ -4,52 +4,71 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdlib.h> 
+#include <signal.h>
 
 struct Mensaje {
-    pid_t pid;        
-    char buf[256];  
+    pid_t pid;
+    char buf[256];
 };
 
 struct Usuario {
     pid_t pid;
-    int fd; 
+    int fd;
 };
 
 int main() {
     mkfifo("/tmp/central.fifo", 0666);
+    mkfifo("/tmp/reporte.fifo", 0666);
 
     int fd_central_in = open("/tmp/central.fifo", O_RDONLY);
+    if (fd_central_in < 0) {
+        perror("Error al abrir central.fifo");
+        return 1;
+    }
 
+    
     struct Usuario usuarios[100];
-    int num = 0; 
-
-    struct Mensaje msg; 
+    int num = 0;
+    struct Mensaje msg;
 
     while (1) {
         int n = read(fd_central_in, &msg, sizeof(msg));
-
         if (n > 0) {
             if (strcmp(msg.buf, "/register") == 0) {
-                // Nuevo usuario
                 char fifo_name[64];
                 sprintf(fifo_name, "/tmp/user_%d.fifo", msg.pid);
-                mkfifo(fifo_name, 0666);
 
                 int fd_user = open(fifo_name, O_WRONLY);
+                if (fd_user < 0) {
+                    perror("Error al abrir fifo del usuario");
+                    continue;
+                }
 
                 usuarios[num].pid = msg.pid;
                 usuarios[num].fd = fd_user;
                 num++;
 
                 printf("Usuario %d registrado\n", msg.pid);
+                fflush(stdout);
+
+            }  else if (strncmp(msg.buf, "/report", 7) == 0) {
+                       pid_t pid_reportado = atoi(msg.buf + 8);  // convierte "1234" en int
+                       printf("[Central] Recibido reporte contra %d\n", pid_reportado);
+                       int fd_rep = open("/tmp/reporte.fifo", O_WRONLY);
+                       if (fd_rep != -1) {
+                        write(fd_rep, &pid_reportado, sizeof(pid_reportado));
+                        close(fd_rep);
+                    }
             } else {
-                // Reenviar mensaje a los demás
-                printf("Usuario (%d): %s\n", msg.pid, msg.buf);
-                   for (int i = 0; i < num; i++) {
+                for (int i = 0; i < num; i++) {
                     if (usuarios[i].pid != msg.pid) {
                         write(usuarios[i].fd, msg.buf, strlen(msg.buf)+1);
                     }
                 }
+                printf("Mensaje de %d: %s\n", msg.pid, msg.buf);
+                fflush(stdout);
             }
         }
     }
